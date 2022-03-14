24c8726bd7117d71bc2ca2bc58507c33dc4e399e
Salvador169 <62758350+Salvador169@users.noreply.github.com>
2022-03-14 17:07:56 +0000

    Update compare.sh

--- lab01/compare.sh
+++ lab01/compare.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-diff -w $1 first_million_primes.txt>/dev/null;REPLY=$?
+diff -w <(head -n $2 $1) <(head -n $2 first_million_primes.txt)>/dev/null;REPLY=$?
+
 if [ ${REPLY} -eq 0 ]
 then
          echo "Files are identical"
 else
          echo "Files are different"
 fi
+
